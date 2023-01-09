90c5b8f946f284c4539979375f480cecd05b535b
Botalov Sergey <89090168690@mail.ru>
2023-01-09 14:04:31 +0500

    parser var #01

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+from queue import Queue
+
+BASE_URL = 'telderi.ru'
+DOMAIN = f'https://{BASE_URL}/'
+URLS_QUEUE = Queue()
+URLS_BASE = set()
+FILTER = {'#', 'search', 'javascript', 'system', 'default', 'None'}
+
+
+def crawler():
+    while True:
+
+        if URLS_QUEUE.qsize() == 0:
+            break
+
+        url = URLS_QUEUE.get()
+        URLS_BASE.add(url)
+
+        response = requests.get(url)
+        response.raise_for_status()
+        print('Scan url ', DOMAIN, 'Status code ', response.status_code)
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        for link in soup.find_all('a'):
+            link = link.get('href')
+            print(link)
+
+            if BASE_URL not in str(link):
+                continue
+            if any(part in str(link) for part in FILTER):
+                continue
+            URLS_QUEUE.put(link)
+
+
+if __name__ == '__main__':
+    URLS_QUEUE.put(DOMAIN)
+    crawler()
