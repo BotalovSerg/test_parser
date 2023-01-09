5075dfeda56f8ecf32059f7fb86f22bb264e96ad
Botalov Sergey <89090168690@mail.ru>
2023-01-09 14:22:58 +0500

    fix crawler v.01

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ BASE_URL = 'telderi.ru'
 DOMAIN = f'https://{BASE_URL}/'
 URLS_QUEUE = Queue()
 URLS_BASE = set()
-FILTER = {'#', 'search', 'javascript', 'system', 'default', 'None'}
+FILTER = {'#', 'search', 'javascript', 'system', 'default', 'None', 'viewBid', '.jpg', 'uploads'}
 
 
 def crawler():
@@ -20,20 +20,24 @@ def crawler():
 
         response = requests.get(url)
         response.raise_for_status()
-        print('Scan url ', DOMAIN, 'Status code ', response.status_code)
+        print('Scan url ', url, 'Status code ', response.status_code)
 
         soup = BeautifulSoup(response.content, 'lxml')
         for link in soup.find_all('a'):
             link = link.get('href')
-            print(link)
+            # print(link)
 
             if BASE_URL not in str(link):
                 continue
             if any(part in str(link) for part in FILTER):
                 continue
             URLS_QUEUE.put(link)
+    return URLS_BASE
 
 
 if __name__ == '__main__':
     URLS_QUEUE.put(DOMAIN)
-    crawler()
+    try:
+        crawler()
+    except requests.HTTPError as error:
+        print(error)
